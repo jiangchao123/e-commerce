c04377419b2cb1a4e7ed510c2e01f2f18516eca2
jiangchao08 <jiangchao08@baidu.com>
2017-01-27 14:51:49 +0800

    数据库设计

---
+++ src/main/db/e-commerce.sql
@@ -0,0 +1,136 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50505
+ Source Host           : localhost
+ Source Database       : e-commerce
+
+ Target Server Type    : MySQL
+ Target Server Version : 50505
+ File Encoding         : utf-8
+
+ Date: 01/27/2017 14:49:30 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `admin`
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `adminname` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `category`
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(50) NOT NULL,
+  `parent_category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `commodity`
+-- ----------------------------
+DROP TABLE IF EXISTS `commodity`;
+CREATE TABLE `commodity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `shop_id` bigint(20) NOT NULL,
+  `commodity_image` varchar(255) DEFAULT NULL,
+  `commodity_name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `count` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `createtime` datetime NOT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `id` (`id`),
+  KEY `shop_id` (`shop_id`),
+  CONSTRAINT `Commodity_FK1` FOREIGN KEY (`shop_id`) REFERENCES `shop` (`id`),
+  CONSTRAINT `Commodity_FK2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `log`
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `order`
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `commodity_id` bigint(20) NOT NULL,
+  `shop_id` bigint(20) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `commodity_id` (`commodity_id`),
+  CONSTRAINT `Order_FK1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `Order_FK2` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `shop`
+-- ----------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `shopname` varchar(50) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `Shop_FK1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `nickname` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `phone` bigint(11) NOT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `school` varchar(30) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+SET FOREIGN_KEY_CHECKS = 1;
