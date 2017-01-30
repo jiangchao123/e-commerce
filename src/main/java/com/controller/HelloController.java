4263eeec370a53a3eca64c52acc7a31e89554225
jiangchao08 <jiangchao08@baidu.com>
2017-01-30 23:30:01 +0800

    用户列表

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import com.entity.UserDO;
+import com.entity.UserDOExample;
+import com.mapper.UserDOMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by jiangchao08 on 16/12/10.
@@ -10,10 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping(value = "/test")
     public String hello(ModelMap modelMap) {
         modelMap.addAttribute("message", "hello world!");
         System.out.println("===========================");
         return "test";
+    }
+
+    @RequestMapping("/userList")
+    public List<UserDO> viewList(ModelMap modelMap) {
+        List<UserDO> users = userDOMapper.selectByExample(new UserDOExample());
+        modelMap.addAttribute("users", users);
+        return users;
     }
 }
--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,21 +1,31 @@
 package com.controller;
 
 import com.entity.User;
+import com.entity.UserDO;
+import com.entity.UserDOExample;
+import com.mapper.UserDOMapper;
 import com.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Created by jiangchao08 on 16/12/5.
  */
-//@EnableAutoConfiguration
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
+
     @Autowired
     private UserMapper mapper;
+
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/{id}")
     public User view(@PathVariable("id") Long id) {
@@ -24,5 +34,13 @@ public class UserController {
 //        user.setName("jiangchao");
         User user = mapper.selectById(1);
         return user;
+    }
+
+    @RequestMapping("/userList")
+    public List<UserDO> viewList(ModelMap modelMap) {
+        List<UserDO> users = userDOMapper.selectByExample(new UserDOExample());
+        modelMap.addAttribute("users", users);
+        modelMap.addAttribute("test", "testABC");
+        return users;
     }
 }
