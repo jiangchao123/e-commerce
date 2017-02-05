c52aa83a7edb45b7e4f08c65efc010693f1bf39e
hwt2016 <66295112@qq.com>
2017-02-05 16:16:04 +0800

    update .gitignore

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.controller;
 
+import com.entity.AdminDO;
+import com.service.AdminService;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by jiangchao08 on 16/12/10.
@@ -10,8 +15,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private AdminService adminService;
+
     @RequestMapping(value = "/index")
     public String hello(ModelMap modelMap) {
         return "index";
     }
+
+    @RequestMapping(value = "/index",method = RequestMethod.POST)
+    public String hello(ModelMap modelMap,AdminDO adminDO) {
+        if(adminDO.getPassword().equals(adminService.selectByAdminName(adminDO.getAdminname()).getPassword())) {
+            modelMap.addAttribute("admin",adminDO);
+            return "index";
+        }
+        else
+            return "login";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String adminLogin(ModelMap modelMap){
+        return "login";
+    }
+
 }
