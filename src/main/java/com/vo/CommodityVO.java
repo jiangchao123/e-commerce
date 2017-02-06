9f3ffec5f82c6c2509a8d664e6eed453ae5e552f
jiangchao08 <jiangchao08@baidu.com>
2017-02-06 14:28:23 +0800

    将分页部分抽成单独vm

---
+++ src/main/java/com/vo/CommodityVO.java
@@ -0,0 +1,30 @@
+package com.vo;
+
+import java.util.Date;
+
+public class CommodityVO {
+
+    private Long id;
+
+    private Long shopId;
+
+    private String shopName;
+
+    private String commodityImage;
+
+    private String commodityName;
+
+    private String description;
+
+    private Long price;
+
+    private Integer count;
+
+    private Integer categoryId;
+
+    private String categoryName;
+
+    private Date createtime;
+
+    private Date updatetime;
+}
